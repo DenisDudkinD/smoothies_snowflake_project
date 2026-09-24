4f76d2e002d32583c065503143aeab16032c2e7a
DenisDudkinD <denis.dudkin.d@gmail.com>
2026-09-24 23:18:15 +0000

    made nutrition information pull from selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,10 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:',
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)  
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     time_to_insert = st.button('Submit Order')
@@ -33,6 +34,3 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
