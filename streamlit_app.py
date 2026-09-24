5ec53423731034237cc67170fe283a116efbff3e
DenisDudkinD <denis.dudkin.d@gmail.com>
2026-09-24 23:11:57 +0000

    Add api call to fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -32,4 +32,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
